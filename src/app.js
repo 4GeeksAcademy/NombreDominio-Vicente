1c1e05a7916c2f70e1e8e4f82fe9dae49667d1cd
VicenteVD <vavetrano@gmail.com>
2025-06-04 10:23:57 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let ext = ['.com', '.es', '.io', '.net']
+
+function generarDominio(pronoun,adj,noun,ext) {
+  
+  let dominios = [];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of ext){
+          dominios.push(`${p}${a}${n}${e}`);
+        }
+      }
+    }
+  }
+  
+  return dominios;
+   
+}
+
+
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("dominio").innerHTML = generarDominio(pronoun,adj,noun,ext).join('<br>');
+  
+  
 };
